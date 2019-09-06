81016d7a540187843bc505821780928372a2bd3c
Roman <Roman.Vengl1@gmail.com>
2019-09-06 19:04:19 +0300

    1commit

--- module-01/task-01.js
+++ module-01/task-01.js
@@ -1,2 +1,9 @@
-const name = 'Shield Generator';
-let price = '1000';
+const name = 'Генератор защитного поля';
+let price = '1000';;
+
+console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
+
+price = '2000';
+
+console.log(`Выбран ${name}, цена за штуку ${price} кредитов`);
+
